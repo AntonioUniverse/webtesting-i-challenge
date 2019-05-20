33bd1f1f07907f2244c5e592408e18804bab2911
Lambda_School_Loaner_93 <antoniounivers@gmail.com>
2019-05-20 19:31:40 -0400

    mvp met

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -6,14 +6,43 @@ module.exports = {
 };
 
 function succeed(item) {
+  let enhancement = item.enhancement
+  const success = enhancement+=1
+  if(enhancement < 20){
+    return {...item, enhancement: success}
+  }
   return { ...item };
 }
 
 function fail(item) {
+  let enhancement = item.enhancement
+  let durability = item.durability
+  // const failLess = durability-=5
+  // const failGreat = durability-=10
+  const leveldown = enhancement-=1
+  if(enhancement < 15){
+    const failLess= durability-=5
+    return{...item, durability:failLess}
+
+  }
+  
+  if(enhancement >= 16) {
+    const failGreat = durability-=10
+    return{ ...item, durability:failGreat, enhancement:leveldown}
+  } 
+ 
+   
+   
+
+
   return { ...item };
 }
 
 function repair(item) {
+  const durability = item.durability
+  if(durability < 100){
+    return{ ...item, durability:100}
+  }
   return { ...item };
 }
 
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,72 @@
 const enhancer = require('./enhancer.js');
 // test away!
+
+describe('repair() method', () => {
+
+
+// repair function to repair durability to 100
+it('Repairs durability to 100', () =>{
+    const item = {
+        name: "Diamond Sword",
+        durability: 10,
+
+    }
+    const fixed = enhancer.repair(item)
+    const repaired = {
+        name: "Diamond Sword",
+        durability:100
+    }
+
+
+    
+    expect(fixed).toStrictEqual(repaired)
+
+})
+})
+
+describe('succeed() method', () => {
+
+
+    // repair function to repair durability to 100
+    it('add enhancement + 1 ', () =>{
+        const item = {
+
+            enhancement:  Math.floor(Math.random() *20),
+    
+        }
+        const enhanced = enhancer.succeed(item)
+        const enhancementsuccess = enhanced
+    
+    
+        
+        expect(enhanced).toEqual(enhancementsuccess)
+    
+    })
+    })
+
+    describe('fail() method', () => {
+
+
+        // repair function to repair durability to 100
+        it(' item less than 15 lose 5 durability, item 15 or more lose 10 durability, item is 16 or more lose 10 durability and 1 enhancement ', () =>{
+            const item = {
+                name: "Diamond Sword",
+                enhancement:17,
+                durability: 100,
+        
+            }
+            const enhancedfail = enhancer.fail(item)
+            const enhancementfail = {
+                name: "Diamond Sword",
+                enhancement:16,
+                durability: 90,
+
+            }
+        
+        
+            
+            expect(enhancedfail).toStrictEqual(enhancementfail)
+        
+        })
+        })
+
